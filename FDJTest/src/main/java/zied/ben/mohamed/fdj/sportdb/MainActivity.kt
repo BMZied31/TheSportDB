7a37d031bd4b45a0efe0cd84e1311fef7ab78643
Zied <zied.ben.med.fr@gmail.com>
2023-04-12 00:23:20 +0200

    🚧 🧱 Add viewBinding as generic type in baseActivity constructor

--- FDJTest/src/main/java/zied/ben/mohamed/fdj/sportdb/MainActivity.kt
+++ FDJTest/src/main/java/zied/ben/mohamed/fdj/sportdb/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import zied.ben.mohamed.fdj.sportdb.base.BaseActivity
+import zied.ben.mohamed.fdj.sportdb.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
-class MainActivity: BaseActivity<MainViewModel>(MainViewModel::class.java) {
+class MainActivity: BaseActivity<MainViewModel, ActivityMainBinding>() {
+
+    override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
 
     override val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.tvHello.text = "First Step 1 Complete :)"
     }
 
     override fun canGoBack(): Boolean {
