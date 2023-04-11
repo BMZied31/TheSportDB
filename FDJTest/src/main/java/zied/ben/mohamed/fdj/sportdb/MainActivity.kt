658016dc87519c0dbec2f7a361519016da3e9097
Zied <zied.ben.med.fr@gmail.com>
2023-04-11 22:22:48 +0200

    🎉 init project

---
+++ FDJTest/src/main/java/zied/ben/mohamed/fdj/sportdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package zied.ben.mohamed.fdj.sportdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
